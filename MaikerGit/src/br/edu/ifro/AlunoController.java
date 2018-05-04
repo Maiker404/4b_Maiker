8ed65b57afacf83eb983375628407ea90bc1dbf2
76220842200 <76220842200@JIPA-LABIN05-06.ifro.local>
2018-05-04 15:18:00 -0400

    Inserindo em Banco de Dados.

--- MaikerGit/src/br/edu/ifro/AlunoController.java
+++ MaikerGit/src/br/edu/ifro/AlunoController.java
@@ -1,27 +1,38 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.edu.ifro;
-
+import br.edu.ifro.modelo.Aluno;
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-
-/**
- * FXML Controller class
- *
- * @author 76220842200
- */
+import javafx.stage.Stage;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 public class AlunoController implements Initializable {
-
-    /**
-     * Initializes the controller class.
-     */
+    @FXML
+    private JFXTextField txtNome;
+    @FXML
+    private JFXButton btnSalvar;
+    @FXML
+    private JFXButton btnFechar;
     @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-    
+    public void initialize(URL url, ResourceBundle rb) {}    
+    @FXML
+    private void gravarNoBanco(ActionEvent event) {
+        EntityManagerFactory emf=Persistence.createEntityManagerFactory("aula");
+        EntityManager em=emf.createEntityManager();
+        Aluno aluno=new Aluno();
+        aluno.setNome(txtNome.getText());
+        em.getTransaction().begin();
+        em.persist(aluno);
+        em.getTransaction().commit();
+    }
+    @FXML
+    private void fecharJanela(ActionEvent event) {
+        Stage stage = (Stage) btnFechar.getScene().getWindow();
+        stage.close();
+    }
 }
