11924b3636a0b628665976905a8179c27558793f
76220842200 <76220842200@JIPA-LABIN05-06.ifro.local>
2018-05-04 15:01:25 -0400

    Criando tela Aluno e abertura de telas em FXML.

--- MaikerGit/src/br/edu/ifro/MenuPrincipalController.java
+++ MaikerGit/src/br/edu/ifro/MenuPrincipalController.java
@@ -7,12 +7,16 @@ package br.edu.ifro;
 
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTextField;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
+import javafx.scene.Scene;
+import javafx.scene.control.MenuItem;
+import javafx.stage.Stage;
 
 /**
  *
@@ -24,12 +28,30 @@ public class MenuPrincipalController implements Initializable {
     @FXML
     private JFXButton btnSoma;
     @FXML
+    private MenuItem menuCadastroAluno;
+    @FXML
+    private MenuItem menuCadastroSair;
+    @FXML
     private void soma(ActionEvent event){
         txtResultado.setText(""+(Integer.parseInt(txtNum1.getText())+Integer.parseInt(txtNum2.getText())));
     }
     @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-    
+    public void initialize(URL url, ResourceBundle rb) {}    
+    @FXML
+    private void abrirCadastroAluno(ActionEvent event) {
+        try{
+            FXMLLoader fxmlLoader = new FXMLLoader();
+            fxmlLoader.setLocation(getClass().getResource("Aluno.fxml"));
+            Scene scene = new Scene(fxmlLoader.load(),900,682);
+            Stage stage = new Stage();
+            stage.setTitle("Cadastrar Aluno");
+            stage.setScene(scene);
+            stage.show();
+        }
+        catch(IOException e){}
+    }
+    @FXML
+    private void fecharPrograma(ActionEvent event) {
+        System.exit(0);
+    }
 }
